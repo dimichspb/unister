98b42c20e4a2556dbbdbf837a41ffda09aa84ea6
Dmitry Tarantin <dimichspb@gmail.com>
2016-06-27 12:01:11 +0300

    Changes to migrations to assign roles to demo users

--- migrations/m160627_074944_adding_demo_users.php
+++ migrations/m160627_074944_adding_demo_users.php
@@ -7,11 +7,16 @@ class m160627_074944_adding_demo_users extends Migration
 {
     public function up()
     {
+        $auth = Yii::$app->authManager;
+
         $demoAdmin = new User();
         $demoAdmin->username = 'admin';
         $demoAdmin->setPassword('admin');
         $demoAdmin->email = 'admin@admin.com';
         $demoAdmin->save();
+
+        $adminRole = $auth->getRole('admin');
+        $auth->assign($adminRole, $demoAdmin->id);
 
         $demoManager = new User();
         $demoManager->username = 'manager';
@@ -19,11 +24,17 @@ class m160627_074944_adding_demo_users extends Migration
         $demoManager->email = 'manager@manager.com';
         $demoManager->save();
 
+        $managerRole = $auth->getRole('manager');
+        $auth->assign($managerRole, $demoManager->id);
+
         $demoCustomer = new User();
         $demoCustomer->username = 'customer';
         $demoCustomer->setPassword('customer');
         $demoCustomer->email = 'customer@customer.com';
         $demoCustomer->save();
+
+        $customerRole = $auth->getRole('customer');
+        $auth->assign($customerRole, $demoCustomer->id);
     }
 
     public function down()
