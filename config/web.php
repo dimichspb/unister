2ffc380d267c045db46d8bdf673993dd70618281
Dmitry Tarantin <dimichspb@gmail.com>
2016-06-27 11:31:51 +0300

    Adding urlmanager short url rules

--- config/web.php
+++ config/web.php
@@ -50,6 +50,13 @@ $config = [
                 'logout' => 'site/logout',
                 'about' => 'site/about',
                 'contact' => 'site/contact',
+
+                'airlines' => 'airline/index',
+
+                '<_c:[\w\-]+>/<_a:[\w\-]+>' => '<_c>/<_a>',
+                '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+                '<_c:[\w\-]+>' => '<_c>/index',
+                '<_c:[\w\-]+>/<_a:[\w\-]+>/<id:\d+>' => '<_c>/<_a>',
             ],
         ],
         'authManager' => [
