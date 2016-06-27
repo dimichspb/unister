b5722a40bd33d8805a20ecad5bb868681717898e
Dmitry Tarantin <dimichspb@gmail.com>
2016-06-27 12:18:05 +0300

    Adding City model

---
+++ models/City.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "city".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $iata
+ */
+class City extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'city';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'iata'], 'required'],
+            [['name', 'iata'], 'string', 'max' => 255],
+            [['name'], 'unique'],
+            [['iata'], 'unique'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'iata' => 'Iata',
+        ];
+    }
+}
